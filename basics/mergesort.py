b6fd9edcecad84d37e52e93a9acbd02233163316
Aku Niskanen <aku.niskanen@tuni.fi>
2019-10-21 01:47:04 -0700

    Strings to integers

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -11,6 +11,15 @@ def debug_print(debug_msg=None, **kwargs):
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
 
-    unsorted_list = input_str.split(",")
+    input_list = input_str.split(",")
+    debug_print(input_list=input_list)
 
-    debug_print(unsorted_list=unsorted_list)
+    value_list = []
+    for x in input_list:
+        try:
+            value_list.append(int(x))
+        except ValueError as err:
+            print("Invalid input.")
+            quit(1)
+
+    debug_print(value_list=value_list)
