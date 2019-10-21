07291091b01a983d256488357b263ddb087d0188
Aku Niskanen <aku.niskanen@tuni.fi>
2019-10-21 02:14:09 -0700

    Removed debug-prints

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -9,12 +9,10 @@ def debug_print(debug_msg=None, **kwargs):
 
 
 def mergesort(array):
-    debug_print(array=array)
     if len(array) <= 1:
         return array
 
     m = len(array) // 2
-    debug_print(m=m)
 
     left = mergesort(array[:m])
     right = mergesort(array[m:])
@@ -23,8 +21,6 @@ def mergesort(array):
 
 
 def merge(left, right):
-    debug_print(debug_msg="Merging...", left=left, right=right)
-
     merged = []
 
     while len(left) > 0 and len(right) > 0:
@@ -38,16 +34,12 @@ def merge(left, right):
     else:
         merged += right
 
-    debug_print(merged=merged)
     return merged
 
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
-
     input_list = input_str.split(",")
-    debug_print(input_list=input_list)
-
     value_list = []
     for x in input_list:
         try:
@@ -55,8 +47,6 @@ if __name__ == "__main__":
         except ValueError as err:
             print("Invalid input.")
             quit(1)
-
-    debug_print(value_list=value_list)
 
     sorted_list = mergesort(value_list)
     print(sorted_list)
